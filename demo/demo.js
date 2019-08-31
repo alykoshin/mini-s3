00a4395e4c25332f8473ba75ceb60b0a39de5876
Alexander <alykoshin@gmail.com>
2019-08-31 23:32:59 +0300

    move ChainableNode to @utilities/object, upd deps

--- demo/demo.js
+++ demo/demo.js
@@ -14,9 +14,14 @@ const test_me = async () => {
   const dir   = './data';
   const fname = dir + '/test.txt';
 
-  //
+  // List buckets
 
-  //await miniS3.buckets().list();
+  const buckets = await miniS3
+    .buckets()
+    .list();
+  console.log('buckets:', buckets);
+
+  // Select bucket by name
 
   const s3bucket = await miniS3
           .bucket(bucket)
@@ -24,25 +29,26 @@ const test_me = async () => {
         //  s3bucket
   ;
 
+  // Delete all keys in the bucket (and list keys to verify)
+
   const deletedKeys = await s3bucket.deleteAll();
   console.log('deletedKeys:', deletedKeys);
+  //await s3bucket.files(filenames).delete();
 
-  //await s3bucket.file(fname1).upload();
-  //await s3bucket.file(markerFile).upload();
+  // Upload all files in directory to the bucket
+
+  ////await s3bucket.file(fname1).upload();
+  ////await s3bucket.file( fname ).upload();
+  ////await s3bucket.file(markerFile).upload();
   await s3bucket.directory(dir).upload();
 
-  //await s3bucket.file( fname ).upload();
-
-  const finalKeys = await s3bucket.listKeys();
-  console.log('finalKeys:', finalKeys);
+  // List keys in the bucket
 
   //const filenames = await s3bucket.listNames();
   //console.log('filenames:', filenames);
-  //
-  ////await asyncForEach(filenames, async fname => await s3bucket.file(fname).delete());
-  //await s3bucket.files(filenames).delete();
-  //
   //await s3bucket.list();
+  const finalKeys = await s3bucket.listKeys();
+  console.log('finalKeys:', finalKeys);
 
   console.log('DONE');
 };
