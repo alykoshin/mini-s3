f469a36762cdf3213dfb9d5d1111f026facd84a5
Alexander <alykoshin@gmail.com>
2020-01-10 22:57:59 +0300

    add remoteDir to _upload as a quick fix

--- demo/demo.js
+++ demo/demo.js
@@ -11,8 +11,11 @@ const test_me = async () => {
     secretAccessKey: credentials.secretAccessKey,
   });
 
-  const dir   = './data';
-  const fname = dir + '/test.txt';
+  const remoteDir = credentials.directory;
+
+  const localDir  = './data';
+  const fname = localDir + '/test.txt';
+
 
   // List buckets
 
@@ -29,15 +32,15 @@ const test_me = async () => {
 
   const s3bucket = await miniS3
           .bucket(bucket)
-        //.then(s3bucket =>
-        //  s3bucket
+        //      //.then(s3bucket =>
+        //      //  s3bucket
   ;
 
   // Delete all keys in the bucket (and list keys to verify)
 
   //const deletedKeys = await s3bucket.deleteAll();
-  const deletedKeys = await s3bucket.deleteFiltered(name => name !== 'marker.txt');
-  console.log('deletedKeys:', deletedKeys);
+  //const deletedKeys = await s3bucket.deleteFiltered(name => name !== 'marker.txt');
+  //console.log('deletedKeys:', deletedKeys);
   //await s3bucket.files(filenames).delete();
 
   // Upload all files in directory to the bucket
@@ -45,16 +48,18 @@ const test_me = async () => {
   ////await s3bucket.file(fname1).upload();
   ////await s3bucket.file( fname ).upload();
   ////await s3bucket.file(markerFile).upload();
-  await s3bucket.directory(dir).upload();
+
+  //await s3bucket.directory(localDir).upload();
+  await miniS3._uploadOne({ filename: fname, bucket, remoteDir: remoteDir });
 
   // List keys in the bucket
 
   //const filenames = await s3bucket.listNames();
   //console.log('filenames:', filenames);
   //await s3bucket.list();
-  const finalKeys = await s3bucket.listKeys();
-  console.log('finalKeys:',        finalKeys);
-  console.log('finalKeys.length:', finalKeys.length);
+  //const finalKeys = await s3bucket.listKeys();
+  //console.log('finalKeys:',        finalKeys);
+  //console.log('finalKeys.length:', finalKeys.length);
 
   console.log('DONE');
 };
